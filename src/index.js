b31dd841518719bcb4ade001a39b31c06f535591
PeterCastillo <peterjackcc@gmail.com>
2023-03-09 17:01:00 -0500

    Swagger config

--- src/index.js
+++ src/index.js
@@ -2,27 +2,37 @@ import mongoose from "mongoose";
 import express from "express";
 import swaggerJSDoc from "swagger-jsdoc";
 import SwaggerUi from "swagger-ui-express";
+import * as path from "path";
 import { usuarioRouter } from "./router/usuarioRouter.js";
+import { fileURLToPath } from "url";
 
-const servidor = express();
-const PORT = process.env.PORT ?? 5000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const swaggerOptions = {
   swaggerDefinition: {
+    openapi: "3.0.0",
     info: {
       title: "Restaurant Manager API",
       description: "Restaurant API information",
       contact: {
         name: "Restaurant Manager",
       },
-      servers: "http://localhost:3000",
+      version: "1.0.0",
+      servers: [
+        {
+          url: "http://localhost:3000",
+        },
+      ],
     },
   },
-  apis: ["index.js"],
+  apis: [`${path.join(__dirname, "./router/*.js")}`],
 };
 
-const swaggerDocs = swaggerJSDoc(swaggerOptions);
+const servidor = express();
+const PORT = process.env.PORT ?? 5000;
 
-servidor.use("/api-docs", SwaggerUi.serve, SwaggerUi.setup(swaggerDocs));
+servidor.use("/api-docs", SwaggerUi.serve, SwaggerUi.setup(swaggerJSDoc(swaggerOptions)));
 // La aplicacion entendera los json provenientes del cliente
 servidor.use(express.json());
 servidor.use(usuarioRouter);
--- src/router/usuarioRouter.js
+++ src/router/usuarioRouter.js
@@ -1,7 +1,51 @@
 import { Router } from "express";
 import { login, registro } from "../controllers/usuarioController.js";
 
-export const usuarioRouter = Router()
+export const usuarioRouter = Router();
 
-usuarioRouter.post("/registro", registro)
-usuarioRouter.post("/login", login)
+//crear usuario
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *      Usuario:
+ *          type: object
+ *          properties:
+ *              nombre:
+ *                  type: string
+ *              correo:
+ *                  type: string
+ *              contrasena:
+ *                  type: string
+ *              accesos: 
+ *                  type:  array
+ *          example:
+ *              nombre: Peter Castillo
+ *              correo: peterjackc@gmail.com
+ *              contrasena: peter0202
+ *              accesos: [] 
+ */
+
+
+/**
+ * @swagger
+ * /registro:
+ *  post:
+ *      summary: registrar un nuevo usuario
+ *      tags: [Usuario]    
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      $ref: '#/components/schemas/Usuario'
+ *      responses:
+ *          201:
+ *              description: nuevo usuario creado
+ *                  
+ */
+
+
+usuarioRouter.post("/registro", registro);
+usuarioRouter.post("/login", login);
